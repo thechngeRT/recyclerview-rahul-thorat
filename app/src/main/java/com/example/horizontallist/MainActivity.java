a16294b5f53c3aedbc526f6707489db03a6145e5
thechngeRT <rahul7083115247@gmail.com>
2020-06-09 18:42:14 +0530

    First commit

--- app/src/main/java/com/example/horizontallist/MainActivity.java
+++ app/src/main/java/com/example/horizontallist/MainActivity.java
@@ -4,104 +4,138 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
 
-    RecyclerView mList1,mList2, mList3, mList4, mList5;
-    List<App> appList1, appList2, appList3, appList4, appList5;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
+public class MainActivity extends AppCompatActivity implements CustomAdaptor.OnItemClickListener {
+
+    GridLayout mainGrid1;
+
+
+    public static final String EXTRA_URL = "imageUrl";
+    public static final String EXTRA_CREATOR = "creatorName";
+
+    private RecyclerView mList1;
+    private CustomAdaptor customAdaptor;
+    private ArrayList<App> appList1;
+    private RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainGrid1=findViewById(R.id.mainGrid);
+
+
         mList1 = findViewById(R.id.list1);
-        mList2 = findViewById(R.id.list2);
-        mList3 = findViewById(R.id.list3);
-        mList4 = findViewById(R.id.list4);
-        mList5 = findViewById(R.id.list5);
+        mList1 = findViewById(R.id.list2);
+        mList1 = findViewById(R.id.list3);
+        mList1 = findViewById(R.id.list4);
+        mList1= findViewById(R.id.list5);
+
+        mList1.setHasFixedSize(true);
+       // **///{** mList1.setHasFixedSize(true);
+
+      //  mList1.setHasFixedSize(true);
+
+       // mList1.setHasFixedSize(true);
+      //  mList1.setHasFixedSize(true);
+
+     //   mList1.setLayoutManager(new LinearLayoutManager(this));
+     //   mList1.setLayoutManager(new LinearLayoutManager(this));
+     //   mList1.setLayoutManager(new LinearLayoutManager(this));
+     //   mList1.setLayoutManager(new LinearLayoutManager(this));
+        mList1.setLayoutManager(new LinearLayoutManager(this));
+
 
         appList1 = new ArrayList<>();
-
-        appList1.add(new App(R.drawable.sandeep_feed,"sandeep-maheshwari", 40));
-        appList1.add(new App(R.drawable.darr," Darr", 40));
-        appList1.add(new App(R.drawable.kyvo,"Kya woh sach", 20));
-        appList1.add(new App(R.drawable.ishq,"Ishq Wala Love", 40));
-        appList1.add(new App(R.drawable.ek,"Ek Purani Kahani", 40));
+      //  appList2 = new ArrayList<>();
+      //  appList3 = new ArrayList<>();
+      //  appList4 = new ArrayList<>();
+      //  appList5 = new ArrayList<>();
 
 
-        LinearLayoutManager manager1 = new LinearLayoutManager(this);
-        manager1.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mList1.setLayoutManager(manager1);
+        mRequestQueue = Volley.newRequestQueue(this);
+        parseJSON();
+    }
 
-        mList2 = findViewById(R.id.list2);
+    private void parseJSON() {
+        String url = "https://d51md7wh0hu8b.cloudfront.net/android/v1/prod/Radio/hi/show.json";
 
-        appList2 = new ArrayList<>();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("data");
 
-        appList2.add(new App(R.drawable.podcast,"Jansatta Podcast", 40));
-        appList2.add(new App(R.drawable.bazzi," Puliyabaazi", 40));
-        appList2.add(new App(R.drawable.story,"The Big Story", 40));
-        appList2.add(new App(R.drawable.mankibat,"man ki bat", 40));
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject data = jsonArray.getJSONObject(i);
 
+                                String creatorName = data.getString("t");
+                                String imageUrl = data.getString("pF");
 
+                                appList1.add(new App(imageUrl, creatorName ));
+                            }
 
-        LinearLayoutManager manager2 = new LinearLayoutManager(this);
-        manager2.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mList2.setLayoutManager(manager2);
+                            customAdaptor = new CustomAdaptor(MainActivity.this, appList1);
+                            mList1.setAdapter(customAdaptor);
+                            customAdaptor.setOnItemClickListener(MainActivity.this);
 
-        appList3 = new ArrayList<>();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
 
-        appList3.add(new App(R.drawable.kno,"GK NOTES", 40));
-        appList3.add(new App(R.drawable.jobs," Jobs Jankari", 40));
-
-        LinearLayoutManager manager3 = new LinearLayoutManager(this);
-        manager3.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mList3.setLayoutManager(manager3);
-
-
-        appList4 = new ArrayList<>();
-
-        appList4.add(new App(R.drawable.ram,"Ramayan", 40));
-        appList4.add(new App(R.drawable.maha," Mahabharat", 40));
-        appList4.add(new App(R.drawable.sadguru,"Sadhguru", 40));
-        appList4.add(new App(R.drawable.githa,"Bhagavad Gita", 40));
-        appList4.add(new App(R.drawable.krishna,"Krishna Wani", 40));
-
-        LinearLayoutManager manager4 = new LinearLayoutManager(this);
-        manager4.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mList4.setLayoutManager(manager4);
-
-        appList5 = new ArrayList<>();
-
-        appList5.add(new App(R.drawable.naved,"RJ Naved", 40));
-        appList5.add(new App(R.drawable.bauuaa," Bauaa", 40));
-
-
-        LinearLayoutManager manager5 = new LinearLayoutManager(this);
-        manager5.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mList5.setLayoutManager(manager5);
+        mRequestQueue.add(request);
+    }
 
 
 
-        CustomAdaptor adaptor1 = new CustomAdaptor(this,appList1);
-        mList1.setAdapter(adaptor1);
 
-        CustomAdaptor adaptor2 = new CustomAdaptor(this,appList2);
-        mList2.setAdapter(adaptor2);
 
-        CustomAdaptor adaptor3 = new CustomAdaptor(this,appList3);
-        mList3.setAdapter(adaptor3);
+    @Override
+    public void onItemClick(int position) {
+        Intent detailIntent = new Intent(this, jsonfech.class);
+        App clickedItem = appList1.get(position);
+        //clickedItem = appList2.get(position);
+       // clickedItem = appList3.get(position);
+       // clickedItem = appList4.get(position);
+        //clickedItem = appList5.get(position);
 
-        CustomAdaptor adaptor4 = new CustomAdaptor(this,appList4);
-        mList4.setAdapter(adaptor4);
 
-        CustomAdaptor adaptor5 = new CustomAdaptor(this,appList5);
-        mList5.setAdapter(adaptor5);
+        detailIntent.putExtra(EXTRA_URL, clickedItem.getImageUrl());
+        detailIntent.putExtra(EXTRA_CREATOR, clickedItem.getCreator());
 
+        startActivity(detailIntent);
 
     }
 }
